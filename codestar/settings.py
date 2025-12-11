dc3030d26d2472c529329d17aac5a71d6bd7272c
Diane-Ring <diane-hawkins@hotmail.co.uk>
2025-12-11 17:09:21 +0000

    install gunicorn, add to requirements, create Procfile, add .herokuapp.com to allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--+*&g@t%13@rfj=ru5=a@j5q$)y%j2usi9zi#d)&fca^c6owm@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com'
+]
 
 
 # Application definition
