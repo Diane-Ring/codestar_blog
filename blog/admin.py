c2cc55ee94f39f45c157f68db4d205cbc977eece
Diane-Ring <diane-hawkins@hotmail.co.uk>
2025-12-15 10:25:36 +0000

    migrate comments models, register new model to admin.py, add comments on admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
+
+class PostAdmin(admin.ModelAdmin):
+	list_display = ("title", "author", "status", "created_on", "updated_on")
+	list_filter = ("status", "created_on", "updated_on")
+	search_fields = ("title", "content", "author__username")
+	prepopulated_fields = {"slug": ("title",)}
+
+
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ("post", "author", "approved", "created_on")
+	list_filter = ("approved", "created_on")
+	search_fields = ("post__title", "author__username", "body")
+
 
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
