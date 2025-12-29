097c29048d93f31e39a6f48a7aafbc89190a8f7c
Diane-Ring <diane-hawkins@hotmail.co.uk>
2025-12-29 10:00:21 +0000

    Add Delete a comment

--- static/js/comments.js
+++ static/js/comments.js
@@ -2,9 +2,12 @@ const editButtons = document.getElementsByClassName("btn-edit");
 const commentText = document.getElementById("id_body");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
+const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+const deleteButtons = document.getElementsByClassName("btn-delete");
+const deleteConfirm = document.getElementById("deleteConfirm");
 
 /** 
- * 
+ * Initializes edit functionality for the provided edit buttons.
  */
 
 for (let button of editButtons) {
@@ -14,5 +17,17 @@ for (let button of editButtons) {
         commentText.value = commentContent;
         submitButton.innerText = "Update";
         commentForm.setAttribute("action", `edit_comment/${commentId}`);
+    });
+}
+
+/**
+ * Initializes deletion functionality for the provided delete buttons.
+ */
+
+for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+        let commentId = e.target.getAttribute("comment_id");
+        deleteConfirm.href = `delete_comment/${commentId}`;
+        deleteModal.show();
     });
 }
